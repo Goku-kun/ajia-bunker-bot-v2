cc95ff0d7a7c0d5751dda7d47aa44ffddb271d06
novaxrono <jethvadharmarajsinh@gmail.com>
2021-03-06 17:18:32 +0530

    ADD: initialized the bot

---
+++ handlerFile.js
@@ -0,0 +1,51 @@
+// primary command handler: all the commands are handled here and then delegated to other specific handlers for separation of concerns.
+function commandHandler(command, args, message) {
+    switch (command) {
+        case "help":
+            helpHandler(message);
+            break;
+        case "":
+            message.channel.send("Hello from the bot!");
+            break;
+        case "server":
+            commandServerHandler(message);
+            break;
+        case "test":
+            testCommandHandler(message, args);
+            break;
+
+        default:
+            message.channel.send(
+                "Command not found. Use `!ajia help` to view the help menu."
+            );
+    }
+}
+
+function commandServerHandler(message) {
+    message.channel.send(`The name of the server is \`${message.guild.name}\``);
+    return;
+}
+
+function helpHandler(message) {
+    message.channel.send(`
+------**Ajia Bunker Bot Guide**------
+
+\`!ajia\`  sends a hello message
+\`!ajia server\`  sends the name of the server
+\`!ajia test foo\`  foo bar testing
+       `);
+}
+
+function testCommandHandler(message, args) {
+    if (!args.length) {
+        return message.channel.send(`No arguments provided.`);
+    }
+
+    if (args[0] == "foo") {
+        return message.channel.send(`bar`);
+    }
+
+    return message.channel.send(args);
+}
+
+module.exports = { commandHandler, commandServerHandler, helpHandler };
---
+++ index.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+const Discord = require("discord.js");
+const { prefix, TOKEN } = require("./config.json");
+const {
+    commandHandler,
+    commandServerHandler,
+    helpHandler,
+} = require("./handlerFile.js");
+
+dotenv.config();
+const client = new Discord.Client();
+
+client.on("message", function messageResponder(message) {
+    if (!message.content.startsWith(`${prefix}`) || message.author.bot) return;
+
+    var args = message.content.slice(prefix.length).trim().split(/ +/);
+    var command = args.shift().toLowerCase();
+
+    commandHandler(command, args, message);
+});
+
+// only runs once the bot is alive.
+client.once("ready", function readCallback() {
+    console.log("Ready");
+});
+
+client.login(TOKEN);
